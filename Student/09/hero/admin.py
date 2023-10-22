c613c40ba05893388b20465df478a67e0605c32e
Tyler Lee <kd0jay@gmail.com>
2023-10-21 19:41:26 -0600

    started project 09 (duplicated project 07 for starting base code)

---
+++ Student/09/hero/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Superhero
+
+class SuperheroAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+    prepopulated_fields = {"slug": ("name",)}
+
+admin.site.register(Superhero, SuperheroAdmin)
