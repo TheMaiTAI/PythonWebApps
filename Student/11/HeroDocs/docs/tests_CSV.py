d0845e85b0baefd72f706f544aa7f8221818897f
Tyler Lee <kd0jay@gmail.com>
2023-11-05 21:13:25 -0700

    renamed tests.py to tests_JSON.py created tests_CSV.py

---
+++ Student/11/HeroDocs/docs/tests_CSV.py
@@ -0,0 +1,6 @@
+import os
+from pathlib import Path
+from .models import Article, Superhero
+from django.test import TestCase
+from json import dump, loads
+
--- Student/11/HeroDocs/docs/tests_JSON.py
+++ Student/11/HeroDocs/docs/tests_JSON.py
@@ -4,16 +4,6 @@ from .models import Article, Superhero
 from django.test import TestCase
 from json import dump, loads
 
-#Misc Tests
-
-class CheckDatabase(TestCase):
-    fixtures = Article.objects.all()
-    num_articles = len(Article.objects.all())
-    def test_database_is_empty(self, num = num_articles):
-        print(num)
-        self.assertEquals(num, 0)
-
-#JSON Tests
 #Heroes
 class AddHeroesJSON(TestCase):
     fixtures = Superhero.objects.all()
